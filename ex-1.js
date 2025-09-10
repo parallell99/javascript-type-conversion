f6518dbb6d397feab3b9bac9b5dfef99c41d2b62
parallell99 <jr.projectx@gmail.com>
2025-09-10 18:02:11 +0700

    re ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,8 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
-depositAmountFromUser = Number(depositAmountFromUser)
 
-accountBalance = accountBalance + depositAmountFromUser;
+accountBalance = accountBalance + Number(depositAmountFromUser);
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
